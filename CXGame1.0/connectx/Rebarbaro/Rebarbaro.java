e948f1cdf387748f07664800723dac9dbfa45cab
chiar0 <fabio.chiarini2@studio.unibo.it>
2023-04-15 17:57:03 +0200

    era uscito male il commit :)
    ho messo pure il gitignore

--- CXGame1.0/connectx/Rebarbaro/Rebarbaro.java
+++ CXGame1.0/connectx/Rebarbaro/Rebarbaro.java
@@ -30,8 +30,109 @@ public class Rebarbaro implements CXPlayer {
 		TIMEOUT = timeout_in_secs;
     }
 
-    
+	public static int minimax(CXBoard B, int depth, int alpha, int beta, boolean maximizingPlayer) {
+		if (depth == 0 || B.checkForWin(1) || B.checkForWin(2) || B.getAvailableColumns().length == 0) {
+			// Leaf node or game over
+			return evaluate(B);
+		}
 
+		if (maximizingPlayer) {
+			// Maximize player 1's score
+			int maxScore = Integer.MIN_VALUE;
+			for (int col : B.getAvailableColumns()) {
+				CXBoard newBoard = B.getDeepCopy();
+				newBoard.makeMove(col, 1);
+				int score = minimax(newBoard, depth - 1, alpha, beta, false);
+				maxScore = Math.max(maxScore, score);
+				alpha = Math.max(alpha, score);
+				if (beta <= alpha) {
+					// Beta cutoff
+					break;
+				}
+			}
+			return maxScore;
+		} else {
+			// Minimize player 2's score
+			int minScore = Integer.MAX_VALUE;
+			for (int col : B.getAvailableColumns()) {
+				CXBoard newBoard = B.getDeepCopy();
+				newBoard.makeMove(col, 2);
+				int score = minimax(newBoard, depth - 1, alpha, beta, true);
+				minScore = Math.min(minScore, score);
+				beta = Math.min(beta, score);
+				if (beta <= alpha) {
+					// Alpha cutoff
+					break;
+				}
+			}
+			return minScore;
+		}
+	}
+
+	public static int evaluate(CXBoard B) {
+		// Evaluate the score of the current board position
+		// This implementation simply counts the number of 1-in-a-row, 2-in-a-row, and 3-in-a-row for each player
+		int[] scores = B.getScores();
+		int score1 = scores[0] + 2 * scores[1] + 10 * scores[2];
+		int score2 = scores[0] + 2 * scores[2] + 10 * scores[4];
+		return score1 - score2;
+	}
+
+	public static int selectColumn(CXBoard B, int depth) {
+		int bestScore = Integer.MIN_VALUE;
+		int bestCol = -1;
+		for (int col : B.getAvailableColumns()) {
+			CXBoard newBoard = B.getDeepCopy();
+			newBoard.makeMove(col, 1);
+			int score = minimax(newBoard, depth - 1, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+			if (score > bestScore) {
+				bestScore = score;
+				bestCol = col;
+			}
+		}
+		return bestCol;
+	}
+
+	public boolean checkForWin(int player) {
+		int[][] directions = {{1, 0}, {0, 1}, {1, 1}, {-1, 1}}; // directions to search for win
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[0].length; col++) {
+				if (board[row][col] == player) {
+					for (int[] dir : directions) {
+						int count = 1; // count number of connected tokens in a direction
+						int r = row + dir[0];
+						int c = col + dir[1];
+						while (r >= 0 && r < board.length && c >= 0 && c < board[0].length && board[r][c] == player) {
+							count++;
+							r += dir[0];
+							c += dir[1];
+						}
+						if (count >= 4) {
+							return true; // found a win
+						}
+					}
+				}
+			}
+		}
+		return false; // no win found
+	}
+	
+	public CXBoard getDeepCopy() {
+		int[][] newGrid = new int[getHeight()][getWidth()];
+	
+		// Copy the grid
+		for (int row = 0; row < getHeight(); row++) {
+			for (int col = 0; col < getWidth(); col++) {
+				newGrid[row][col] = getCell(row, col);
+			}
+		}
+	
+		// Copy the other properties
+		CXBoard newBoard = new CXBoard(newGrid, getNextPlayer(), getLastMove(), getNumMoves());
+	
+		return newBoard;
+	}
+	
     /**
 	 * Selects a free colum on game board.
 	 * <p>
@@ -40,7 +141,7 @@ public class Rebarbaro implements CXPlayer {
 	 * cases do not apply, selects a random column.
 	 * </p>
 	 */
-	public int selectColumn(CXBoard B) {
+/* 	public int selectColumn(CXBoard B) {
 		START = System.currentTimeMillis(); // Save starting time
 
 		Integer[] L = B.getAvailableColumns();
@@ -56,7 +157,7 @@ public class Rebarbaro implements CXPlayer {
 			System.err.println("Timeout!!! Random column selected");
 			return save;
 		}
-	}
+	} */
 
 	private void checktime() throws TimeoutException {
 		if ((System.currentTimeMillis() - START) / 1000.0 >= TIMEOUT * (99.0 / 100.0))
