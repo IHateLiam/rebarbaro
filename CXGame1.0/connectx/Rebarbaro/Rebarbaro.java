115bf97a2dde98b93101164b9de481b18a91f3c6
chiar0 <fabio.chiarini2@studio.unibo.it>
2023-04-26 19:23:18 +0200

    Revert "Merge remote-tracking branch 'origin/trapbin' into trapbin"
    This reverts commit 7215bccee95640cf07dd65d57fbaf09a20fc6dfa.

--- CXGame1.0/connectx/Rebarbaro/Rebarbaro.java
+++ CXGame1.0/connectx/Rebarbaro/Rebarbaro.java
@@ -30,48 +30,9 @@ public class Rebarbaro implements CXPlayer {
 		TIMEOUT = timeout_in_secs;
     }
 
-	public int selectColumn(CXBoard B) {
-		START = System.currentTimeMillis();		
-		int bestScore = Integer.MIN_VALUE;
-		int bestCol = -1;
-		int depth = 4;  //depth nei parametri di selectColumn non va bene perchE' java a quanto pare vuole che i parametri siano gli stessi di CXPlayer.selectColumn(..)
+	public int minimax(CXBoard B, int depth, int alpha, int beta, boolean maximizingPlayer) {
 		Integer[] L = B.getAvailableColumns();
-
-		for (int col : L) {
-			int score = minimax(B, depth, col, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
-			if (score > bestScore) {
-				bestScore = score;
-				bestCol = col;
-			}
-			
-			}
-
-		if (bestCol == -1) {
-			try {
-				bestCol = singleMoveBlock(B, L);
-			} catch(TimeoutException e) {
-				System.err.println("Timeout!!! singleMoveBlock ritorna -1 in selectColumn");
-			}
-		}
-		return bestCol;
-	}
-
-	public int minimax(CXBoard B, int depth, int firstMove, int alpha, int beta, boolean maximizingPlayer) {
-		Integer[] L = B.getAvailableColumns();
-		CXGameState state = B.markColumn(firstMove);       //marcamento numero 1 
-		L = B.getAvailableColumns();
-
-		if (state == yourWin) {
-			B.unmarkColumn();
-			return maximizingPlayer ? -1 : 1;
-		}
-		
-		if (state == myWin) {
-			B.unmarkColumn();
-			return maximizingPlayer ? 1 : -1;
-		}
-
-		
+		//if (depth == 0 || B.checkForWin(1) || B.checkForWin(2) || B.getAvailableColumns().length == 0) {
 		int winningColumn = -1;
 		try {
 			winningColumn = singleMoveWin(B, L);
@@ -80,15 +41,8 @@ public class Rebarbaro implements CXPlayer {
 			winningColumn = -1;
 		}
 		if (depth == 0 || winningColumn != -1 || L.length == 0) {
-			// Leaf node or game over		
-			if(state == myWin) {
-				B.unmarkColumn();                         //smarcamento numero 1 condizionale livello foglia 
-				return 1;
-			}
-			else {
-				B.unmarkColumn();                        //smarcamento numero 1 condizionale (else) livello foglia
-				return 0;
-			}
+			// Leaf node or game over
+			return evaluate(B);
 		}
 
 
@@ -96,15 +50,14 @@ public class Rebarbaro implements CXPlayer {
 			// Maximize player 1's score
 			int maxScore = Integer.MIN_VALUE;
 			for (int col : L) {
+				//CXBoard newBoard = B.getDeepCopy();
+				//newBoard.makeMove(col, 1);
 
-				state = B.markColumn(col);                 //marcamento numero 2
-				if (state == myWin) {
-					B.unmarkColumn();					//smarcamento condizionale numero 2
-					return 1;
-				}
-				B.unmarkColumn();                      //smarcamento condizionale (else) numero 2
-				int score = minimax(B, depth - 1, col, alpha, beta, false);
-				
+				B.markColumn(col);
+				int score = minimax(B, depth - 1, alpha, beta, false);
+				B.unmarkColumn();
+
+				//int score = minimax(newBoard, depth - 1, alpha, beta, false);
 				maxScore = Math.max(maxScore, score);
 				alpha = Math.max(alpha, score);
 				if (beta <= alpha) {
@@ -112,22 +65,19 @@ public class Rebarbaro implements CXPlayer {
 					break;
 				}
 			}
-
-
-			B.unmarkColumn();                              //smarcamento condizionale numero 1 nodo generico
 			return maxScore;
 		} else {
 			// Minimize player 2's score
 			int minScore = Integer.MAX_VALUE;
 			for (int col : L) {
-				state = B.markColumn(col);
-				if (state == myWin) {
-					B.unmarkColumn();
-					return 1;
-				}
-				B.unmarkColumn();
-				int score = -minimax(B, depth - 1, col, alpha, beta, true);
+				//CXBoard newBoard = B.getDeepCopy();
+				//newBoard.makeMove(col, 2);
 
+				B.markColumn(col);
+				int score = -minimax(B, depth - 1, alpha, beta, true);
+				B.unmarkColumn();
+				
+				//int score = minimax(newBoard, depth - 1, alpha, beta, true);
 				minScore = Math.min(minScore, score);
 				beta = Math.min(beta, score);
 				if (beta <= alpha) {
@@ -135,23 +85,20 @@ public class Rebarbaro implements CXPlayer {
 					break;
 				}
 			}
-
-			B.unmarkColumn();                              //smarcamento condizionale (else) numero 1 nodo generico
 			return minScore;
 		}
 	}
 
-	
-	/*
-	* 
-	// Evaluate the score of the current board position
-	// This implementation simply counts the number of 1-in-a-row, 2-in-a-row, and 3-in-a-row for each player
-	int[] scores = B.getScores();
-	int score1 = scores[0] + 2 * scores[1] + 10 * scores[2];
-	int score2 = scores[0] + 2 * scores[2] + 10 * scores[4];
-	return score1 - score2;
-	*/
 	public int evaluate(CXBoard B) {
+		/*
+		 * 
+		 // Evaluate the score of the current board position
+		 // This implementation simply counts the number of 1-in-a-row, 2-in-a-row, and 3-in-a-row for each player
+		 int[] scores = B.getScores();
+		 int score1 = scores[0] + 2 * scores[1] + 10 * scores[2];
+		 int score2 = scores[0] + 2 * scores[2] + 10 * scores[4];
+		 return score1 - score2;
+		 */
 		Integer[] L = B.getAvailableColumns();
 
 		int winningColumn = -1;
@@ -169,89 +116,91 @@ public class Rebarbaro implements CXPlayer {
 		}
 	}    
 
-	
+	public int selectColumn(CXBoard B) {
+		START = System.currentTimeMillis();
 
-	private void checktime() throws TimeoutException {
-		if ((System.currentTimeMillis() - START) / 1000.0 >= TIMEOUT * (99.0 / 100.0))
-			throw new TimeoutException();
-	}
+		int bestScore = Integer.MIN_VALUE;
+		int bestCol = -1;
+		int depth = 1;  //depth nei parametri di selectColumn non va bene perchE' java a quanto pare vuole che i parametri siano gli stessi di CXPlayer.selectColumn(..)
+		Integer[] L = B.getAvailableColumns();
 
-	/**
-	 * Check if we can win in a single move
-	 *
-	 * Returns the winning column if there is one, otherwise -1
-	 */	
-	private int singleMoveWin(CXBoard B, Integer[] L) throws TimeoutException {
-		for(int i : L) {
-			checktime(); // Check timeout at every iteration
-			CXGameState state = B.markColumn(i);
-			if (state == myWin){
-				B.unmarkColumn();
-				return i; // Winning column found: return immediately
+		for (int col : L) {
+			//CXBoard newBoard = B.getDeepCopy();
+			//newBoard.makeMove(col, 1);
+			int score = minimax(B, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+			if (score > bestScore) {
+				bestScore = score;
+				bestCol = col;
 			}
-			B.unmarkColumn();
 		}
-			return -1;
+
+		if (bestCol == -1) {
+			try {
+				bestCol = singleMoveBlock(B, L);
+			} catch(TimeoutException e) {
+				System.err.println("Timeout!!! singleMoveBlock ritorna -1 in selectColumn");
+			}
+		}
+		return bestCol;
 	}
 
-	/**
-	 * Check if we can block adversary's victory 
-   *
-   * Returns a blocking column if there is one, otherwise a random one
-   */
-	private int singleMoveBlock(CXBoard B, Integer[] L) throws TimeoutException {
-		TreeSet<Integer> T = new TreeSet<Integer>(); // We collect here safe column indexes
-
-		for(int i : L) {
-			checktime();
-			T.add(i); // We consider column i as a possible move
-			B.markColumn(i);         //marcamento numero (1)
-			
-			int j;
-			boolean stop;
-
-			for(j = 0, stop=false; j < L.length && !stop; j++) {
-				//try {Thread.sleep((int)(0.2*1000*TIMEOUT));} catch (Exception e) {} // Uncomment to test timeout
-				checktime();
-				if(!B.fullColumn(L[j])) {
-					CXGameState state = B.markColumn(L[j]);     //marcamento numero (2)
-					if (state == yourWin) {
-						T.remove(i); // We ignore the i-th column as a possible move
-						stop = true; // We don't need to check more
+	//esiste singleMoveWin() del prof che funziona meglio
+	//quella cambia che invece che ritornare un booleano ti ritorna la colonna se puoi vincere altrimenti -1
+	/*
+	 * 
+	 public boolean checkForWin(int player) {    
+		 int[][] directions = {{1, 0}, {0, 1}, {1, 1}, {-1, 1}}; // directions to search for win
+		 for (int row = 0; row < board.length; row++) {
+			 for (int col = 0; col < board[0].length; col++) {
+				if (board[row][col] == player) {
+					for (int[] dir : directions) {
+						int count = 1; // count number of connected tokens in a direction
+						int r = row + dir[0];
+						int c = col + dir[1];
+						while (r >= 0 && r < board.length && c >= 0 && c < board[0].length && board[r][c] == player) {
+							count++;
+							r += dir[0];
+							c += dir[1];
+						}
+						if (count >= 4) {
+							return true; // found a win
+						}
 					}
-					B.unmarkColumn(); //Unmark L[j] column (2) 
 				}
 			}
-			B.unmarkColumn();        //smarcamento numero (1)
 		}
-
-		if (T.size() > 0) {
-			Integer[] X = T.toArray(new Integer[T.size()]);
-            return X[X.length / 2];    //central columns are better
+		return false; // no win found
+	}
+	*/
+	
+	//non ci serve copiare una nuova tabella quando possiamo direttamente fare markcolumn e e unmarkcolumn e vedere il gamestate direttamente dalla tabella originale
+	/*
+	 * 
+	 public CXBoard getDeepCopy() {
+		 int[][] newGrid = new int[getHeight()][getWidth()];
+		 
+		 // Copy the grid
+		 for (int row = 0; row < getHeight(); row++) {
+			 for (int col = 0; col < getWidth(); col++) {
+				 newGrid[row][col] = getCell(row, col);
+				}
+			}
 			
-		} else {
-			return L[rand.nextInt(L.length)];
+			// Copy the other properties
+			CXBoard newBoard = new CXBoard(newGrid, getNextPlayer(), getLastMove(), getNumMoves());
+			
+			return newBoard;
 		}
-	}
-
-	public String playerName() {
-		return "Rebarbarosessopazzofollesgravato";
-	}
-}
-
-
-
-					
-					
+		*/
 /*
-	* 
-	private int[] getScores(CXBoard B, int player) {
-		int[] scores = new int[L];
-		
-		for (int col = 0; col < B.getN(); col++) {
-			if (B.fullColumn(col)) {
-				scores[col] = -1;
-				continue;
+ * 
+ private int[] getScores(CXBoard B, int player) {
+	 int[] scores = new int[L];
+	 
+	 for (int col = 0; col < B.getN(); col++) {
+		 if (B.fullColumn(col)) {
+			 scores[col] = -1;
+			 continue;
 			}
 			
 			// Place the piece in the column
@@ -321,4 +270,94 @@ public class Rebarbaro implements CXPlayer {
 	}
 	*/
 	
-	
+    /**
+	 * Selects a free colum on game board.
+	 * <p>
+	 * Selects a winning column (if any), otherwise selects a column (if any) 
+	 * that prevents the adversary to win with his next move. If both previous
+	 * cases do not apply, selects a random column.
+	 * </p>
+	 */
+	/* 	public int selectColumn(CXBoard B) {
+		START = System.currentTimeMillis(); // Save starting time
+		
+		Integer[] L = B.getAvailableColumns();
+		int save    = L[rand.nextInt(L.length)]; // Save a random column 
+		
+		try {
+			int col = singleMoveWin(B,L);
+			if(col != -1) 
+			return col;
+			else
+			return singleMoveBlock(B,L);
+		} catch (TimeoutException e) {
+			System.err.println("Timeout!!! Random column selected");
+			return save;
+		}
+	} */
+
+	private void checktime() throws TimeoutException {
+		if ((System.currentTimeMillis() - START) / 1000.0 >= TIMEOUT * (99.0 / 100.0))
+			throw new TimeoutException();
+	}
+
+	/**
+	 * Check if we can win in a single move
+	 *
+	 * Returns the winning column if there is one, otherwise -1
+	 */	
+	private int singleMoveWin(CXBoard B, Integer[] L) throws TimeoutException {
+    for(int i : L) {
+			checktime(); // Check timeout at every iteration
+      CXGameState state = B.markColumn(i);
+      if (state == myWin)
+        return i; // Winning column found: return immediately
+      B.unmarkColumn();
+    }
+		return -1;
+	}
+
+	/**
+   * Check if we can block adversary's victory 
+   *
+   * Returns a blocking column if there is one, otherwise a random one
+   */
+	private int singleMoveBlock(CXBoard B, Integer[] L) throws TimeoutException {
+		TreeSet<Integer> T = new TreeSet<Integer>(); // We collect here safe column indexes
+
+		for(int i : L) {
+			checktime();
+			T.add(i); // We consider column i as a possible move
+			B.markColumn(i);
+
+			int j;
+			boolean stop;
+
+			for(j = 0, stop=false; j < L.length && !stop; j++) {
+				//try {Thread.sleep((int)(0.2*1000*TIMEOUT));} catch (Exception e) {} // Uncomment to test timeout
+				checktime();
+				if(!B.fullColumn(L[j])) {
+					CXGameState state = B.markColumn(L[j]);
+					if (state == yourWin) {
+						T.remove(i); // We ignore the i-th column as a possible move
+						stop = true; // We don't need to check more
+					}
+					B.unmarkColumn(); // 
+				}
+			}
+			B.unmarkColumn();
+		}
+
+		if (T.size() > 0) {
+			Integer[] X = T.toArray(new Integer[T.size()]);
+            return X[X.length / 2];    //central columns are better
+			
+		} else {
+			return L[rand.nextInt(L.length)];
+		}
+	}
+
+	public String playerName() {
+		return "Rebarbaro";
+	}
+}
